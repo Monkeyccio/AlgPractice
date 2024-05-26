2ea1837c2cf6bb057c198f6440fd5d75e6482783
cdjccio <cdjccio@foxmail.com>
2024-05-26 21:44:06 +0800

    add test file

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "include\sum.h"
+
+using namespace std;
+int main()
+{
+    int a = 10, b = 20;
+    int val = sum( a, b );
+    cout << val << endl;
+}
